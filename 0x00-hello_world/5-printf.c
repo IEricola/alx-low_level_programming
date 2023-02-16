a78079e7a7f26f05b4f75f0d7e9283e57cd5bd0e
IEricola <akineric99@gmail.com>
2023-02-16 13:42:52 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - controls program execution
+ *
+ * Return: returns control to the calling function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
