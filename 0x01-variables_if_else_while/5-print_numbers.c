240262fd847425dd49b884e38b55ed35e1a4f342
IEricola <akineric99@gmail.com>
2023-02-17 13:14:31 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entr point of action
+ *
+ * Return: Is always 0, if successful
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
