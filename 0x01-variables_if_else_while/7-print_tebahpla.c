02be5a30a8cb9247866df81047c9daa579d12a4a
IEricola <akineric99@gmail.com>
2023-02-17 13:28:50 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of action
+ *
+ * Return: Is always o, if successful
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
