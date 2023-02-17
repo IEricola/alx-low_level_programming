ea591a3da110321643f7fa990c57067c5544503c
IEricola <akineric99@gmail.com>
2023-02-17 14:00:08 -0800

    Zeroth task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,23 +5,23 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - Entry point fir action
+ * main - Entry point of action
  *
- * Return: Is 0 if successful
+ * Return: Is always 0, if successful
  */
 
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-        if (n > 0)
-                printf("%d is positive\n", n);
-        else if (n == 0)
-                printf("%d is zero\n", n);
-        else
-                printf("%d is negative\n", n);
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
 }
