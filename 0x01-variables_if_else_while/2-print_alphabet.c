7fbf0e3c1d02788fdbc2d00e385cd34d718a5bcf
IEricola <akineric99@gmail.com>
2023-02-17 12:19:44 -0800

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,19 +9,11 @@ int main(void)
 {
 	char c;
 
-	char d;
-
 	c = 'a';
-	d = 'A';
 	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
-		}
-	while
-		(d <= 'Z') {
-			putchar(d);
-			d++;
 		}
 	putchar('\n');
 	return (0);
