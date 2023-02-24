f6045b5cc9a103d6a5f32c7b99e2a39c51dbf80b
IEricola <akineric99@gmail.com>
2023-02-23 16:10:52 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies iteger
+ * @x: The 1st number to ultiply
+ * @y: The 2nd num to multipky with 1st num
+ * Return: Result of multiplying x and y
+ */
+
+int mul(int x, int y)
+
+{
+	return (x * y);
+}
