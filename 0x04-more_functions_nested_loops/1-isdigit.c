3c1227862a0a284ae48169dc7738f203bcba415d
IEricola <akineric99@gmail.com>
2023-02-23 16:04:20 -0800

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if aum is a digit
+ * @x: The umber to be checked
+ * Return: 1 for a character that will be adigit or 0 for ay else
+ */
+
+int _isdigit(int x)
+{
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
