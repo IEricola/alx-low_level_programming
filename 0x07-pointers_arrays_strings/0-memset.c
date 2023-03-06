5db2457696fc3ff9f9517aadca1e45e0cb3209a1
IEricola <akineric99@gmail.com>
2023-03-06 14:16:00 -0800

    0 Memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - function declaration
+ * @s: pointer to constant
+ * @b: constant
+ * @n: number of bytes
+ * Return: pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
